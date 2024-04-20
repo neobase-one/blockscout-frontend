41bb53000947498fd8ceeacc52e5f567086e42ca
Alex <aesshoferi@gmail.com>
2024-04-20 14:26:38 +0330

    fix: fix app config

--- configs/app/app.ts
+++ configs/app/app.ts
@@ -1,14 +1,26 @@
 import { getEnvValue } from './utils';
 
+const calculateAppHost = () => {
+  if (getEnvValue('NEXT_PUBLIC_VERCEL_ENV')) {
+    switch (getEnvValue('NEXT_PUBLIC_VERCEL_ENV')) {
+      case 'preview':
+        return getEnvValue('NEXT_PUBLIC_VERCEL_BRANCH_URL');
+      case 'production':
+        return getEnvValue('NEXT_PUBLIC_VERCEL_PRODUCTION_URL');
+      default:
+        return getEnvValue('NEXT_PUBLIC_VERCEL_URL');
+    }
+  } else {
+    return getEnvValue('NEXT_PUBLIC_APP_HOST');
+  }
+};
+
 const appPort = getEnvValue('NEXT_PUBLIC_APP_PORT');
 const appSchema = getEnvValue('NEXT_PUBLIC_APP_PROTOCOL');
-const appHost = getEnvValue('NEXT_PUBLIC_APP_HOST');
-const baseUrl = [
-  appSchema || 'https',
-  '://',
-  appHost,
-  appPort && ':' + appPort,
-].filter(Boolean).join('');
+const appHost = calculateAppHost();
+const baseUrl = [ appSchema || 'https', '://', appHost, appPort && ':' + appPort ]
+  .filter(Boolean)
+  .join('');
 const isDev = getEnvValue('NEXT_PUBLIC_APP_ENV') === 'development';
 
 const app = Object.freeze({
@@ -18,6 +30,7 @@ const app = Object.freeze({
   port: appPort,
   baseUrl,
   useProxy: getEnvValue('NEXT_PUBLIC_USE_NEXT_JS_PROXY') === 'true',
+  blankRasaRFC: getEnvValue('NEXT_PUBLIC_BLANK_RASA_RFC'),
 });
 
 export default app;
